7eb843e9c03d3753ceecf14d9e57a679af5421b9
Muhammet Ozcan <ozcanmuhammet@gmail.com>
2019-03-24 16:08:11 +0300

    validations

--- models/Director.js
+++ models/Director.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const DirectorSchema = new Schema({
-    name: String,
+    name: {
+        type: String,
+        maxlength: 50,
+        minlength: 2
+    },
     surname: String,
     bio: String,
     createDate: {
--- models/Movie.js
+++ models/Movie.js
@@ -4,11 +4,17 @@ const Schema = mongoose.Schema;
 const MovieSchema = new Schema({
     title: {
         type: String,
-        required: true
+        required: [true, '`{PATH}` alanı zorunludur.'],
+        maxlength: [25, '`{PATH}` alanı (`{VALUE}`) {MAXLENGTH} karakterden küçük olmalıdır.'],
+        minlength: [1, '`{PATH}` alanı (`{VALUE}`) {MINLENGTH} karakterden büyük olmalıdır.']
     },
     category: String,
     country: String,
-    year: Number,
+    year: {
+        type: Number,
+        max: 2020,
+        min: 1900
+    },
     imdbScore: Number,
     directorId: Schema.Types.ObjectId,
     createDate: {
